81bf6a4540e71d588002b98859327c709eec315d
Nikola Zubic <nikolazubic97@yahoo.com>
2019-11-30 20:51:41 +0100

    Project structure - fix

---
+++ Backend/src/main/java/xml/web/services/team2/sciXiv/SciXivApplication.java
@@ -0,0 +1,13 @@
+package xml.web.services.team2.sciXiv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SciXivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SciXivApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/xml/web/services/team2/sciXiv/SciXivApplicationTests.java
@@ -0,0 +1,13 @@
+package xml.web.services.team2.sciXiv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SciXivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

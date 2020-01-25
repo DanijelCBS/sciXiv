10384a540b092a10f6b6d6f0e30aef0474822bc0
Nikola Zubic <nikolazubic97@yahoo.com>
2020-01-25 04:21:07 +0100

    Successfully created Notification Template xml file and repoRetrieve

---
+++ Backend/src/main/java/xml/web/services/team2/sciXiv/repository/NotificationRepository.java
@@ -0,0 +1,43 @@
+package xml.web.services.team2.sciXiv.repository;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.springframework.stereotype.Repository;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import xml.web.services.team2.sciXiv.utils.dom.DOMParser;
+
+@Repository
+public class NotificationRepository {
+
+	private static final String notificationXsdSchemaPath = "src/main/resources/static/xmlSchemas/notification.xsd";
+	private static final String notificationTemplate = "src/main/resources/static/templates/notificationTemplate.xml";
+
+	public static Document retrieveNotification() throws IOException, SAXException, ParserConfigurationException {
+		// readFile(NotificationTemplate)
+		File f = new File(notificationTemplate);
+		BufferedReader br = new BufferedReader(new FileReader(f));
+		StringBuilder sb = new StringBuilder("");
+		String line;
+
+		while ((line = br.readLine()) != null) {
+			sb.append(line);
+		}
+
+		br.close();
+
+		String notificationStr = sb.toString();
+
+		Document document = DOMParser.buildDocument(notificationStr, notificationXsdSchemaPath);
+
+		return document;
+	}
+
+}
